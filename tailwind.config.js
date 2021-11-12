31ac50fd7539f01fcf902a91f1d3a612c75efb2f
Jean Kalo <jean.kalo@student.rocva.nl>
2021-11-12 14:07:35 +0100

    Bol.com Remake - Tailwind CSS V1

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+    ...colors,
+    spotify: '#1DB954',
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
